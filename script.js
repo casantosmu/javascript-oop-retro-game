554d869f5e37b28ac46447fbdb99d3382fec5c93
Carlos Santos <casantosmu@gmail.com>
2023-11-06 18:03:45 +0100

    Add projectile, projectiles pool and player shoot

--- script.js
+++ script.js
@@ -35,11 +35,24 @@ class Player {
     if (this.game.keys.includes("ArrowRight")) {
       this.x += this.speed;
     }
+    if (this.game.keys.includes("1")) {
+      this.shoot();
+    }
 
-    if (this.x < 0) {
-      this.x = 0;
-    } else if (this.x > this.game.width - this.width) {
-      this.x = this.game.width - this.width;
+    if (this.x < -this.width * 0.5) {
+      this.x = -this.width * 0.5;
+    } else if (this.x > this.game.width - this.width * 0.5) {
+      this.x = this.game.width - this.width * 0.5;
+    }
+  }
+
+  /**
+   * Shoot projectile
+   */
+  shoot() {
+    const projectile = this.game.getProjectile();
+    if (projectile) {
+      projectile.start(this.x + this.width * 0.5, this.y);
     }
   }
 }
@@ -47,7 +60,60 @@ class Player {
 /**
  * Class that handles the projectiles that players shoot.
  */
-class Projectile {}
+class Projectile {
+  constructor() {
+    this.width = 4;
+    this.height = 20;
+    this.x = 0;
+    this.y = 0;
+    this.speed = 20;
+    /**
+     * If true is sitting in the pool to be use
+     * If false we pool it from the pool and is not available
+     */
+    this.free = true;
+  }
+
+  /**
+   * @param {CanvasRenderingContext2D} context - Reference to context canvas
+   */
+  draw(context) {
+    if (!this.free) {
+      context.fillRect(this.x, this.y, this.width, this.height);
+    }
+  }
+
+  /**
+   * Update position of the projectile
+   */
+  update() {
+    if (!this.free) {
+      this.y -= this.speed;
+
+      if (this.y < -this.height) {
+        this.reset();
+      }
+    }
+  }
+
+  /**
+   * When it's taken from the pool
+   * @param {number} x - x coordinate of the projectile
+   * @param {number} y - y coordinate of the projectile
+   */
+  start(x, y) {
+    this.x = x - this.width * 0.5;
+    this.y = y;
+    this.free = false;
+  }
+
+  /**
+   * When it's return back to the pool
+   */
+  reset() {
+    this.free = true;
+  }
+}
 
 /**
  * Class that draws and animates space invaders in the game.
@@ -65,8 +131,16 @@ class Game {
     this.canvas = canvas;
     this.width = this.canvas.width;
     this.height = this.canvas.height;
+
+    /** @type {string[]} */
     this.keys = [];
+
     this.player = new Player(this);
+
+    /** @type {Projectile[]} */
+    this.projectilesPool = [];
+    this.numberOfProjectiles = 10;
+    this.#createProjectiles();
 
     window.addEventListener("keydown", (event) => {
       if (!this.keys.includes(event.key)) {
@@ -90,6 +164,31 @@ class Game {
   render(context) {
     this.player.draw(context);
     this.player.update();
+
+    for (const projectile of this.projectilesPool) {
+      projectile.update();
+      projectile.draw(context);
+    }
+  }
+
+  /**
+   * Fill the pool with projectiles at once for performance reasons
+   */
+  #createProjectiles() {
+    for (let i = 0; i < this.numberOfProjectiles; i++) {
+      this.projectilesPool.push(new Projectile());
+    }
+  }
+
+  /**
+   * Taken one free projectile from the pool
+   */
+  getProjectile() {
+    for (const projectile of this.projectilesPool) {
+      if (projectile.free) {
+        return projectile;
+      }
+    }
   }
 }
 
